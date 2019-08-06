f22cb00b660d17958423e78844e598a621b9232f
ThomasHoniball <honiballthomas@gmail.com>
2019-08-06 12:15:19 +0200

    And so it begins

---
+++ Fault_Tolerance.java
@@ -0,0 +1,11 @@
+import packages.lanterna.terminal.*;//Include Lanterna terminal implementation
+
+public class Fault_Tolerance
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+
+}
+
